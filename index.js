731c0dad56c4e7bded278e186c4c6962247c71ec
Samir Roy <rsamir@gmail.com>
2023-07-19 19:10:35 +0530

    v2.0.0 - Add prettier

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: [
     'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
   ],
@@ -8,30 +10,51 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
+    indent: 'off', // managed by prettier
     'import/extensions': 'off',
     'import/first': 'error',
-    'import/no-anonymous-default-export': ['error', {
-      allowArray: false,
-      allowArrowFunction: false,
-      allowAnonymousClass: false,
-      allowAnonymousFunction: false,
-      allowLiteral: false,
-      allowObject: false,
-    }],
+    'import/no-anonymous-default-export': [
+      'error',
+      {
+        allowArray: false,
+        allowArrowFunction: false,
+        allowAnonymousClass: false,
+        allowAnonymousFunction: false,
+        allowLiteral: false,
+        allowObject: false,
+      },
+    ],
     'import/no-extraneous-dependencies': 'off',
     'import/no-mutable-exports': 'error',
     'import/no-unresolved': 'off',
     'import/no-useless-path-segments': 'error',
     'no-use-before-define': 'off',
     'object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
-    'max-len': ['error', {
-      code: 120,
-      ignoreRegExpLiterals: true,
-      ignoreTrailingComments: true,
-      ignorePattern: "(^\\s*?const .* = [\"`'].*[\"`'];$)|(^import.*;$)",
-    }],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreRegExpLiterals: true,
+        ignoreTrailingComments: true,
+        ignorePattern: '(^\\s*?const .* = ["`\'].*["`\'];$)|(^import.*;$)',
+      },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        bracketSpacing: true,
+        bracketSameLine: false,
+        printWidth: 120,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        useTabs: false,
+      },
+    ],
     'react/no-unused-prop-types': 'error',
     'react/jsx-filename-extension': 'off',
     'react/jsx-props-no-spreading': 'off',
@@ -60,25 +83,34 @@ module.exports = {
     '@typescript-eslint/keyword-spacing': ['error', { before: true, after: true }],
     '@typescript-eslint/adjacent-overload-signatures': 'error',
     '@typescript-eslint/array-type': ['error', { default: 'array' }],
-    '@typescript-eslint/consistent-type-assertions': ['error', {
-      assertionStyle: 'as',
-      objectLiteralTypeAssertions: 'allow',
-    }],
-    '@typescript-eslint/consistent-type-imports': ['error', {
-      prefer: 'type-imports',
-      disallowTypeAnnotations: true,
-    }],
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: true,
+      },
+    ],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/member-delimiter-style': ['error', {
-      multiline: {
-        delimiter: 'semi',
-        requireLast: true,
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
       },
-      singleline: {
-        delimiter: 'semi',
-        requireLast: false,
-      },
-    }],
+    ],
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-unnecessary-type-constraint': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
